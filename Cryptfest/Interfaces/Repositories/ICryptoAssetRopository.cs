c978dd4a1d2ee39dfd7d90eebaab9217a8ee9dba
SashaIl <sashailyuk@gmail.com>
2025-09-23 15:11:33 +0200

    Add method GetAssetBySymbolAsync

---
+++ Cryptfest/Interfaces/Repositories/ICryptoAssetRopository.cs
@@ -0,0 +1,11 @@
+﻿using API.Data.Entities.Wallet;
+using Cryptfest.Data.Entities.Api;
+
+namespace Cryptfest.Interfaces.Repositories;
+
+public interface ICryptoAssetRopository
+{
+    Task<List<CryptoAssetInfo>> GetCryptoAssetsAsync();
+    ApiAccess GetApiAccess();
+    Task<CryptoAssetInfo?> GetCryptoAssetBySymbolAsync(string symbol);
+}
---
+++ Cryptfest/Repositories/CryptoAssetRepository.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Data.Entities.Wallet;
+using Cryptfest.Data.Entities.Api;
+using Cryptfest.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cryptfest.Repositories
+{
+    public class CryptoAssetRepository : ICryptoAssetRopository
+    {
+        private readonly ApplicationContext _context;
+
+        public CryptoAssetRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CryptoAssetInfo>> GetCryptoAssetsAsync()
+        {
+            List<CryptoAssetInfo> output = await _context.CryptoAssetInfo.ToListAsync();
+            return output;
+        }
+        public ApiAccess GetApiAccess()
+        {
+            ApiAccess output = _context.ApiAccess.ToList().First() ;
+            return output;
+        }
+
+        public async Task<CryptoAssetInfo?> GetCryptoAssetBySymbolAsync(string symbol)
+        {
+            CryptoAssetInfo? output = await _context.CryptoAssetInfo.FirstOrDefaultAsync(x => x.Symbol == symbol);
+            return output;
+        }
+    }
+}
